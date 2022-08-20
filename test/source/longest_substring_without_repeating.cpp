494f3345941a946de4f0e0da3f4ce809f7d0b02f
bibincnair <bibincnair1@gmail.com>
2022-08-20 21:35:55 +0800

    Longest Substring update

--- test/source/longest_substring_without_repeating.cpp
+++ test/source/longest_substring_without_repeating.cpp
@@ -1,19 +1,21 @@
-#include <vector>
 #include "leetcode/longest_substring_without_repeating.h"
+
+#include <vector>
+
 #include "gtest/gtest.h"
 
-TEST(TestLongestString, Test1){
-    LongestSubstringWithoutRepeat obj;
-    std::string input("abcabcbb");
-    int output = 3;
-    int max_len = obj.lengthOfLongestSubstring(input);
-    EXPECT_EQ(max_len, output);
+TEST(TestLongestString, Test1) {
+  LongestSubstringWithoutRepeat obj;
+  std::string input("abcabcbb");
+  int output = 3;
+  int max_len = obj.lengthOfLongestSubstring(input);
+  EXPECT_EQ(max_len, output);
 }
 
-TEST(TestLongestString, Test2){
-    LongestSubstringWithoutRepeat obj;
-    std::string input("bbbbb");
-    int output = 1;
-    int max_len = obj.lengthOfLongestSubstring(input);
-    EXPECT_EQ(max_len, output);
+TEST(TestLongestString, Test2) {
+  LongestSubstringWithoutRepeat obj;
+  std::string input("bbbbb");
+  int output = 1;
+  int max_len = obj.lengthOfLongestSubstring(input);
+  EXPECT_EQ(max_len, output);
 }
